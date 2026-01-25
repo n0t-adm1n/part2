43921c63d02538b7bb1fa4cbfa8dee9be02f8b34
N0tAdm1n <ahmed29shamir@gmail.com>
2026-01-25 18:34:57 +0530

    Done exercises 2.6, 2.7

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,12 +1,48 @@
 import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`)
+      return ;
+    }
+
+    const nameObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(nameObject));
+    setNewName("");
+  }
+
+  const handleNameChange= (e) => {
+    
+    setNewName(e.target.value);
+  }
 
   return (
-    <>
-      <h1>Hello worlr</h1>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index)=><p key={index}>{person.name}</p>
+      )}
+    </div>
   )
 }
+
 
 export default App
