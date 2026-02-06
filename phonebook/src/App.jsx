1215fbefbca10abf02d862c41c045c46af9f1e9d
N0tAdm1n <ahmed29shamir@gmail.com>
2026-02-06 23:59:54 +0530

    Done exercise 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter';
 import Form from './components/Form';
 import Person from './components/Person';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('')
+
+//fetching data from db
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  },[])
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
